ad00c176be801bb8595ffea334613a609c02159f
Seung Hyun Sung <86032100+DrawDot@users.noreply.github.com>
2021-10-10 20:00:34 +0900

    add kNN prolog script

---
+++ kNN/00_Scripts/prolog.R
@@ -0,0 +1,31 @@
+################################################################################
+# PROLOG 
+################################################################################
+# ML topic : k Nearest Neighbour Algorithm 
+# Project  : Diabetes diagnostic Prediction Problem 
+# Data     : Diabetes in Pima Indian Women from MASS Package 
+# Author   : Seung Hyun Sung 
+# Created  : 10/10/2021 
+# Latest   :  
+################################################################################
+
+# Create directories 
+library(fs)
+dir_create("kNN")
+
+getwd()
+setwd("/Users/seunghyunsung/Desktop/R_DS/Github/DataScience-Supervised-ML/kNN/")
+
+make_project_dir <- function() {
+  dir_names <- c("00_Data",
+                 "00_Scripts",
+                 "01_Business_Understanding",
+                 "02_Visualisation",
+                 "03_Functions_iteration",
+                 "04_Modelling",
+                 "05_Evaluation")
+  dir_create(dir_names)
+  
+  dir_ls()
+}
+make_project_dir()
